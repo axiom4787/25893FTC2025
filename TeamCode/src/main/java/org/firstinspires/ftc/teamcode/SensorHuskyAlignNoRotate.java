224fa812fa4bd8d85a508222a8df276238343a97
Acorn Studios <openlight441@gmail.com>
2025-09-16 17:18:05 -0500

    Limelight Testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorHuskyAlignNoRotate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorHuskyAlignNoRotate.java
@@ -60,7 +60,7 @@ import java.util.concurrent.TimeUnit;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
-@TeleOp(name = "HL Alignme Without Rotate", group = "Auto")
+@TeleOp(name = "HL Align-me Without Rotate", group = "Auto")
 public class SensorHuskyAlignNoRotate extends LinearOpMode {
 
     private final int READ_PERIOD = 50; // ms
